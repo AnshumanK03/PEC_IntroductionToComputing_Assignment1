5a35b75e4fed69cb77d0035c042aeedd00dc503b
AnshumanK03 <96840424+AnshumanK03@users.noreply.github.com>
2022-01-11 09:24:34 +0530

    Update 2q.py

--- 2q.py
+++ 2q.py
@@ -1,14 +1,19 @@
-import math #importing math library for round function
+import math  #importing math library for round function
+standeduction = 10000   #Value of standard deduction in dollars
+dependentdeduc = 3000   #Value of dependent deduction in dollars
 
 print("Enter the gross income")
 grossincome = float(input())
-grossrounded = round(grossincome)#rounds the gross income to nearest integer
+grossrounded = round(grossincome)  #rounds the gross income to nearest integer
 
 print("Enter the number of dependents")
 dependents = int(input())
 
-taxableincome = grossrounded - 10000 - (3000 * dependents)
+taxableincome = grossrounded - standeduction - (dependentdeduc * dependents) #Calculatng the taxable income
 tax = taxableincome * 20/100 
 
-print("Income Tax is %i." %tax)
-print(grossrounded)
+if tax>0 :
+ print("Income Tax is $%i." %tax)
+else:
+  tax=0
+  print("Income Tax exempted, Tax is $0")
